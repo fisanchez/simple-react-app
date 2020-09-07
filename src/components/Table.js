46fcf48886173819c905a132d89cca6d353a4075
Sanchez <fsanchez@1usmfsanchez1.local>
2020-09-06 23:34:09 -0400

    Rendering table entries. Only displaying full_name for now
    Todo next will be caching results for each search term.

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,46 +1,60 @@
-import React from 'react'
-import { fetchPopularRepos } from '../utils/api'
+import React from "react";
+import { fetchPopularRepos } from "../utils/api";
+
+const Item = (props) => {
+  return <p>{props.value}</p>;
+};
+
+const ItemList = (props) => {
+  const results = props.results;
+  console.log("RESULTS", results);
+  const resultList = results.map((result) => {
+    return <Item key={result.full_name} value={result.full_name} />;
+  });
+  return <div>{resultList}</div>;
+};
 
 export default class Table extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.state = {
-      searchValue: '',
-      results: null
-    }
+      searchValue: "",
+      results: null,
+    };
 
-    this.handleChange = this.handleChange.bind(this)
-    this.keyPress = this.keyPress.bind(this)
+    this.handleChange = this.handleChange.bind(this);
+    this.keyPress = this.keyPress.bind(this);
   }
 
-  handleChange(e){
-    console.log('NEW VALUE', e.target.value)
+  handleChange(e) {
     this.setState({ searchValue: e.target.value });
   }
 
-  keyPress(e){
+  keyPress(e) {
     // Search term and clear on pressing Enter key
-    if(e.keyCode === 13){
-      fetchPopularRepos(this.state.searchValue).then((res) => this.setState({results: res}))
+    if (e.keyCode === 13) {
+      fetchPopularRepos(this.state.searchValue).then((res) =>
+        this.setState({ results: res })
+      );
     }
   }
 
   isLoading() {
-    return this.state.results === null
+    return this.state.results === null;
   }
 
   render() {
-    return(
+    return (
       <div>
-        < input 
-            value={this.state.searchValue} 
-            onKeyDown={this.keyPress} 
-            onChange={this.handleChange} 
+        <input
+          value={this.state.searchValue}
+          onKeyDown={this.keyPress}
+          onChange={this.handleChange}
         />
-        { this.isLoading() && <h1> Loading </h1> }
-        { !this.isLoading() && <p> { JSON.stringify(this.state.results) }</p>}
+        {this.isLoading() && <h1> Loading </h1>}
+        {!this.isLoading() && <ItemList results={this.state.results} />}
       </div>
-    )
+    );
   }
 }
